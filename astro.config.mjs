aba3427abdce8c5587f2f2ca25301edea41a19b9
Simon Liang <simon@x-tech.io>
2025-01-04 11:12:03 +0800

    cleanup site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.divby0.io',
+    site: 'https://astro-etch.pages.dev',
     integrations: [mdx(), sitemap(), tailwind(), react()],
 });
